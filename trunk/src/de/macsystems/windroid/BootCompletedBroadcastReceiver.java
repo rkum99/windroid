7786856823294748082ee766125a2089c32f4b29
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2009-07-30 03:42:48 +0000

    using SpotService String Constants.

--- trunk/src/de/macsystems/windroid/BootCompletedBroadcastReceiver.java
+++ trunk/src/de/macsystems/windroid/BootCompletedBroadcastReceiver.java
@@ -1,6 +1,7 @@
 package de.macsystems.windroid;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,18 +9,24 @@ import android.util.Log;
 
 /**
  * @author Jens Hohl
- * @version $Id$
+ * @version $Id: BootCompletedBroadcastReceiver.java 2 2009-07-28 02:15:38Z
+ *          jens.hohl $
  * 
  */
 public class BootCompletedBroadcastReceiver extends BroadcastReceiver
 {
 
-	private final static String LOG_TAG = "BootCompleted";
+	private final static String LOG_TAG = "BootCompletedReceiver";
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context,
+	 * android.content.Intent)
+	 */
 	@Override
 	public void onReceive(final Context _context, final Intent _intent)
 	{
-
 		Log.d(LOG_TAG, "BootCompletedBroadcastReceiver#onRecieve(...) called.");
 
 		if (Intent.ACTION_BOOT_COMPLETED.equals(_intent.getAction()))
@@ -28,17 +35,21 @@ public class BootCompletedBroadcastReceiver extends BroadcastReceiver
 
 			final SharedPreferences preferences = Util.getSharedPreferences(_context);
 			final boolean isLaunchOnBoot = Util.isLaunchOnBoot(preferences);
-
-			Log.i(LOG_TAG, "Launch on boot :" + isLaunchOnBoot);
-
+			Log.i(LOG_TAG, "Start SpotService on boot :" + isLaunchOnBoot);
 			if (isLaunchOnBoot)
 			{
 				final Intent startServiceIntent = new Intent();
-				startServiceIntent.setAction("de.macsystems.windroid.START_SPOT_SERVICE_ACTION");
-				_context.startService(startServiceIntent);
-				Log.i(LOG_TAG, "Service on boot launched through Action :" + startServiceIntent.getAction());
+				startServiceIntent.setAction(SpotService.DE_MACSYSTEMS_WINDROID_START_SPOT_SERVICE_ACTION);
+				final ComponentName name = _context.startService(startServiceIntent);
+				if (name == null)
+				{
+					Log.e(LOG_TAG, "Failed to start SpotService.");
+				}
+				else
+				{
+					Log.i(LOG_TAG, "SpotService on boot launched.");
+				}
 			}
 		}
-
 	}
 }

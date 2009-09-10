c47ff7f3b6142955ba23c8bc039e2cfb5657338b
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2009-09-10 01:42:02 +0000

    Added Activities which allows user to see when update happen or error while update in background Service.

---
+++ trunk/src/de/macsystems/windroid/OngoingUpdate.java
@@ -0,0 +1,25 @@
+package de.macsystems.windroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author mac
+ * @version $Id$
+ * 
+ */
+public class OngoingUpdate extends Activity
+{
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ongoingupdate);
+	}
+}
---
+++ trunk/src/de/macsystems/windroid/UpdateProblem.java
@@ -0,0 +1,24 @@
+package de.macsystems.windroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author mac
+ * @version $Id
+ * 
+ */
+public class UpdateProblem extends Activity
+{
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}

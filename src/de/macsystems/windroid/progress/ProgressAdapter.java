5c2f00ddf43802a37c2c86393d710d6660de96a6
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2009-10-02 05:32:00 +0000

    Adapter Classes which decouples progress monitoring more from code which works on that 'data'

---
+++ src/de/macsystems/windroid/progress/ProgressAdapter.java
@@ -0,0 +1,52 @@
+package de.macsystems.windroid.progress;
+
+import android.app.ProgressDialog;
+
+/**
+ * Adapter Class to decouple classes which want to update a progress progress more
+ * flexible.
+ * 
+ * @author mac
+ * @version $Id$
+ */
+public class ProgressAdapter implements IProgress
+{
+	private final ProgressDialog delegate;
+
+	/**
+	 * 
+	 * @param _dialog
+	 */
+	public ProgressAdapter(ProgressDialog _dialog)
+	{
+		if (_dialog == null)
+		{
+			throw new NullPointerException();
+
+		}
+		delegate = _dialog;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.macsystems.windroid.IProgress#increment()
+	 */
+	@Override
+	public void increment()
+	{
+		delegate.incrementProgressBy(1);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.macsystems.windroid.IProgress#incrementBy(int)
+	 */
+	@Override
+	public void incrementBy(int value)
+	{
+		delegate.incrementProgressBy(value);
+	}
+
+}

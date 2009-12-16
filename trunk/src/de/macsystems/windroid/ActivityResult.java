b9b56c67d0fe1a62d2a7124157d1df67625378a4
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2009-12-16 03:48:33 +0000

    Added TODO Node.

--- trunk/src/de/macsystems/windroid/ActivityResult.java
+++ trunk/src/de/macsystems/windroid/ActivityResult.java
@@ -1,6 +1,9 @@
 package de.macsystems.windroid;
 
 /**
+ * TODO: Seems class is useles as the SDK provides already numbers for return
+ * states.
+ * 
  * @author mac
  * @version $Id$
  */
@@ -13,10 +16,16 @@ public final class ActivityResult
 	/**
 	 * 
 	 */
-	public final static int SUCCESS = 1;
+	public final static int SUCCESS = 1000;
 
 	/**
 	 * 
 	 */
-	public final static int FAILED = 2;
+	public final static int FAILED = 2000;
+
+	/**
+	 * 
+	 */
+	public final static int CANCELED = 3000;
+
 }

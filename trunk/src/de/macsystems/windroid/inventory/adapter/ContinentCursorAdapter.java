5e3105da869bd0999b109704490de39bc2a72f74
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2009-12-19 14:56:08 +0000

    Clean up.

--- trunk/src/de/macsystems/windroid/inventory/adapter/ContinentCursorAdapter.java
+++ trunk/src/de/macsystems/windroid/inventory/adapter/ContinentCursorAdapter.java
@@ -1,7 +1,6 @@
 package de.macsystems.windroid.inventory.adapter;
 
 import android.database.Cursor;
-import android.util.Log;
 import android.view.View;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -16,7 +15,6 @@ public class ContinentCursorAdapter implements SimpleCursorAdapter.ViewBinder
 
 	private static final int CONTINENT_ID_COLUMN = 1;
 	private static final int CONTINENT_NAME_COLUMN = 2;
-	boolean retval = false;
 
 	/*
 	 * (non-Javadoc)
@@ -28,8 +26,6 @@ public class ContinentCursorAdapter implements SimpleCursorAdapter.ViewBinder
 	@Override
 	public boolean setViewValue(final View view, final Cursor cursor, final int columnIndex)
 	{
-		Log.d("", "columnIndex = " + columnIndex);
-		Log.d("", "view = " + view);
 		if (columnIndex == CONTINENT_ID_COLUMN)
 		{
 			final String name = cursor.getString(CONTINENT_ID_COLUMN);

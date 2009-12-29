6c6745b313c8a49049331148c34fc66a5887b65c
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2009-12-29 15:09:08 +0000

    removed logging.

--- trunk/src/de/macsystems/windroid/io/task/UpdateSpotsTask.java
+++ trunk/src/de/macsystems/windroid/io/task/UpdateSpotsTask.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.concurrent.Callable;
 
 import android.content.Context;
-import android.util.Log;
 import de.macsystems.windroid.SpotConfigurationVO;
 import de.macsystems.windroid.WindUtils;
 import de.macsystems.windroid.forecast.Forecast;
@@ -22,6 +21,7 @@ import de.macsystems.windroid.progress.NullProgressAdapter;
 public class UpdateSpotsTask implements Callable<Forecast>
 {
 
+	@SuppressWarnings("unused")
 	private final static String LOG_TAG = UpdateSpotsTask.class.getSimpleName();
 
 	final SpotConfigurationVO spot;
@@ -54,7 +54,6 @@ public class UpdateSpotsTask implements Callable<Forecast>
 	{
 		if (!IOUtils.isNetworkReachable(context))
 		{
-			Log.d(LOG_TAG, "Skipping update, Network not reachable.");
 			throw new RetryLaterException("Skipping update, Network not reachable.");
 		}
 

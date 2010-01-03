4a3c7f8a1849e7cf0fc4668e804530318cd26bfa
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2010-01-03 03:10:47 +0000

    added context menu to SpotOverview Activity. Also worked on better layout for Items shown in list. Also added EN / DE Values for Context menu. ISelectedDAO returns active state of a spot.

--- src/de/macsystems/windroid/db/sqlite/SelectedImpl.java
+++ src/de/macsystems/windroid/db/sqlite/SelectedImpl.java
@@ -1,8 +1,8 @@
 package de.macsystems.windroid.db.sqlite;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 import de.macsystems.windroid.SpotConfigurationVO;
 import de.macsystems.windroid.db.ISelectedDAO;
 import de.macsystems.windroid.io.IOUtils;
@@ -41,39 +41,90 @@ public class SelectedImpl extends BaseImpl implements ISelectedDAO
 	 * .SpotConfigurationVO)
 	 */
 	@Override
-	public void insertSpot(final SpotConfigurationVO _vo)
+	public long insertSpot(final SpotConfigurationVO _vo)
 	{
 		if (_vo == null)
 		{
 			throw new NullPointerException("SpotConfigurationVO");
 		}
 
+		long newID = -1;
 		// TODO : At the moment all spots are activ !!!
-
-		// CREATE TABLE IF NOT EXISTS selected (_id INTEGER PRIMARY KEY
-		// AUTOINCREMENT, spotid text NOT NULL, activ BOOLEAN, usedirection
-		// BOOLEAN, starting TEXT, till TEXT, windmeasure TEXT NOT NULL, minwind
-		// INTEGER, maxwind INTEGER);
 		final SQLiteDatabase db = getWritableDatabase();
 		try
 		{
 			final ContentValues values = new ContentValues();
-			values.put("spotid", _vo.getStation().getId());
-			values.put("name", _vo.getStation().getName());
-			values.put("activ", true);
-			values.put("usedirection", _vo.isUseWindirection());
-			values.put("starting", _vo.getFromDirection().getId());
-			values.put("till", _vo.getToDirection().getId());
-			values.put("windmeasure", _vo.getPreferredWindUnit().name());
-			values.put("minwind", _vo.getWindspeedMin());
-			values.put("maxwind", _vo.getWindspeedMax());
-
-			final long newID = db.insert(tableName, null, values);
-			Log.d("Bla", "New ID = " + newID);
+			values.put(COLUMN_SPOTID, _vo.getStation().getId());
+			values.put(COLUMN_NAME, _vo.getStation().getName());
+			values.put(COLUMN_ACTIV, true);
+			values.put(COLUMN_USEDIRECTION, _vo.isUseWindirection());
+			values.put(COLUMN_STARTING, _vo.getFromDirection().getId());
+			values.put(COLUMN_ID, _vo.getToDirection().getId());
+			values.put(COLUMN_WINDMEASURE, _vo.getPreferredWindUnit().name());
+			values.put(COLUMN_MINWIND, _vo.getWindspeedMin());
+			values.put(COLUMN_MAXWIND, _vo.getWindspeedMax());
+			//
+			newID = db.insert(tableName, null, values);
 		}
 		finally
 		{
 			IOUtils.close(db);
 		}
+
+		return newID;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.macsystems.windroid.db.ISelectedDAO#setActiv(long, boolean)
+	 */
+	public void setActiv(final long _id, final boolean isActiv)
+	{
+		final SQLiteDatabase db = getWritableDatabase();
+		try
+		{
+			final ContentValues values = new ContentValues();
+			values.put(COLUMN_ACTIV, isActiv);
+
+			db.update(tableName, values, "_id=?", new String[]
+			{ "" + _id });
+		}
+		finally
+		{
+			IOUtils.close(db);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.macsystems.windroid.db.ISelectedDAO#isActiv(long)
+	 */
+	@Override
+	public boolean isActiv(long _id)
+	{
+		final SQLiteDatabase db = getReadableDatabase();
+		Cursor c = null;
+		boolean result = false;
+		try
+		{
+			c = db.query(tableName, null, "_id=?", new String[]
+			{ "" + _id }, null, null, null);
+
+			if (!c.moveToFirst())
+			{
+				throw new IllegalArgumentException("_id not found :" + _id);
+			}
+
+			final int index = c.getColumnIndexOrThrow(COLUMN_ACTIV);
+			result = convertIntToBoolean(c.getInt(index));
+		}
+		finally
+		{
+			IOUtils.close(c);
+			IOUtils.close(db);
+		}
+		return result;
 	}
 }
--- src/de/macsystems/windroid/forecast/SpotOverview.java
+++ src/de/macsystems/windroid/forecast/SpotOverview.java
@@ -3,6 +3,13 @@ package de.macsystems.windroid.forecast;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnCreateContextMenuListener;
+import android.view.View.OnLongClickListener;
 import android.widget.SimpleCursorAdapter;
 import de.macsystems.windroid.R;
 import de.macsystems.windroid.db.DAOFactory;
@@ -17,6 +24,10 @@ import de.macsystems.windroid.db.ISelectedDAO;
  */
 public class SpotOverview extends ListActivity
 {
+	private final static String LOG_TAG = SpotOverview.class.getSimpleName();
+	private static final int EDIT_ID = 0;
+	private static final int DELETE_ID = 1;
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -40,5 +51,43 @@ public class SpotOverview extends ListActivity
 				to);
 		shows.setViewBinder(new SpotOverviewViewBinder());
 		setListAdapter(shows);
+		//
+		getListView().setOnCreateContextMenuListener(new OnCreateContextMenuListener()
+		{
+			@Override
+			public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+			{
+				Log.d(LOG_TAG, "public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)");
+//				menu.setHeaderTitle("ContextMenu");
+
+				final long selectedItemID = getSelectedItemId();
+				Log.d(LOG_TAG, "selected ID = " + selectedItemID);
+				boolean isActiv = dao.isActiv(selectedItemID);
+				menu.add(0, 0, 0, "Bearbeiten");
+				if (isActiv)
+				{
+					menu.add(0, 1, 0, R.string.spot_overview_spot_monitoring_disable);
+				}
+				else
+				{
+					menu.add(0, 1, 0, R.string.spot_overview_spot_monitoring_enable);
+				}
+			}
+		});
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		Log.d(LOG_TAG, "On Create Options Menu");
+		menu.add("Disable");
+		menu.add("Ensable");
+		return super.onCreateOptionsMenu(menu);
+	}
+
 }

5e6ec989c73bdb5b0e7a223ab91c26f8f8ff2d65
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2010-02-01 03:45:48 +0000

    added hashCode and equals.

--- src/de/macsystems/windroid/identifyable/Continent.java
+++ src/de/macsystems/windroid/identifyable/Continent.java
@@ -107,4 +107,77 @@ public class Continent implements IdentifyAble, Node<Country>
 		return countrys.isEmpty();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((countrys == null) ? 0 : countrys.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (getClass() != obj.getClass())
+		{
+			return false;
+		}
+		Continent other = (Continent) obj;
+		if (countrys == null)
+		{
+			if (other.countrys != null)
+			{
+				return false;
+			}
+		}
+		else if (!countrys.equals(other.countrys))
+		{
+			return false;
+		}
+		if (id == null)
+		{
+			if (other.id != null)
+			{
+				return false;
+			}
+		}
+		else if (!id.equals(other.id))
+		{
+			return false;
+		}
+		if (name == null)
+		{
+			if (other.name != null)
+			{
+				return false;
+			}
+		}
+		else if (!name.equals(other.name))
+		{
+			return false;
+		}
+		return true;
+	}
+
 }

9d342cbc512e7654cb788aa13e9597cdd1504554
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2010-02-15 09:45:45 +0000

    Allows debugging messages to be turned on/off.

---
+++ src/de/macsystems/windroid/Logging.java
@@ -0,0 +1,21 @@
+package de.macsystems.windroid;
+
+/**
+ * Simple Class which allows easy Logging on/off. Do not use this for Logging
+ * Errors or warnings. just for debugging testing purposes.
+ * 
+ * @author mac
+ * @version $Id$
+ */
+public final class Logging
+{
+	/**
+	 * Returns <code>true</code> if logging is turned on.
+	 * 
+	 * @return
+	 */
+	public final static boolean isLogging()
+	{
+		return true;
+	}
+}

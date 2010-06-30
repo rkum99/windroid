ee76606f1b323f01df693536b3a2b2d408c47573
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2010-06-30 12:37:26 +0000

    Introduced no_sound playback option.

--- branches/release_0.9.1.1/src/de/macsystems/windroid/io/task/AudioFeedbackTask.java
+++ branches/release_0.9.1.1/src/de/macsystems/windroid/io/task/AudioFeedbackTask.java
@@ -53,6 +53,10 @@ import de.macsystems.windroid.io.IOUtils;
  */
 public abstract class AudioFeedbackTask extends AbstractNotificationTask<Void> implements OnCompletionListener
 {
+	/**
+	 * ResID which is used to playback no sound.
+	 */
+	public final static int NO_SOUND_RES_ID = 137;
 
 	private final String LOG_TAG = AudioFeedbackTask.class.getSimpleName();
 
@@ -124,7 +128,7 @@ public abstract class AudioFeedbackTask extends AbstractNotificationTask<Void> i
 	 */
 	private void play(final int _resourceID)
 	{
-		if (player != null)
+		if (player != null && _resourceID != NO_SOUND_RES_ID)
 		{
 
 			AssetFileDescriptor afd = null;

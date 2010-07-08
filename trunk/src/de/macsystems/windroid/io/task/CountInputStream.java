b49bc76ba52a30e758e333569de4feedc876fc4e
jens.hohl <jens.hohl@4d68a910-7ad1-11de-8a13-ad0aff062799>
2010-07-08 00:11:06 +0000

    moved calc into block.

--- trunk/src/de/macsystems/windroid/io/task/CountInputStream.java
+++ trunk/src/de/macsystems/windroid/io/task/CountInputStream.java
@@ -64,13 +64,12 @@ public final class CountInputStream extends FilterInputStream
 		finally
 		{
 			isClosed = true;
-			final float kbs = bytes / 1024.0f;
-			final float mb = kbs / 1024.0f;
 			if (Logging.isEnabled())
 			{
+				final float kbs = bytes / 1024.0f;
+				final float mb = kbs / 1024.0f;
 				Log.d(LOG_TAG, "Read :" + bytes + " Byte, " + kbs + " kB, " + mb + " MB");
 			}
 		}
 	}
-
 }
